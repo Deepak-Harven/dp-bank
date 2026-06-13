88b17156e45a41ef07ad89c013776facfc0ca311
Deepak Harven <1deepakkumarpal@gmail.com>
2026-06-13 11:05:07 +0530

    Support DATABASE_PATH environment variable for SQLite persistence

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const db = new sqlite3.Database('./database.db', (err) => {
+const db = new sqlite3.Database(process.env.DATABASE_PATH || './database.db', (err) => {
     if (err) {
         console.error('Error opening database:', err.message);
     } else {
